a2fc07fe1550f95909f73c089d080526f77d1978
Ajlik <bbgf05436@gmail.com>
2023-06-22 23:48:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,17 @@
+﻿
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int i, s;
+    s = 0;
+    for (i = 1; i <= 1000; i++)
+    {
+        s = s + i;
+    }
+    cout << "Сумма чисел от 1 до 1000 равна:" << s << endl;
+    return 0;
+}
+    
